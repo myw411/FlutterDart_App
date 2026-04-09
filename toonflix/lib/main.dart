5330ef93a599e51e95b9284623e58d98a584fd48
YeoWon <myw411@naver.com>
2026-04-09 19:34:19 +0900

    timer UI, webtoon UI
    timer UI 코드는 노션에 있음
    깜빡하고 commit안 함
    webtoon UI의 API 받아오기까지 함

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,69 +1,20 @@
 import 'package:flutter/material.dart';
-
-class Player {
-  String? name;
-
-  Player({required this.name});
-}
+import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
 
 void main() {
   runApp(App());
 }
 
 //UI부분
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-// 데이터 부분
-class _AppState extends State<App> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
-          ),
-        ),
-      ),
-      home: Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MyLargeTitle(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 40,
-        // 위에 them으로 작성한 내용을 context를 이렇게 연결함
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      // home_screen의 파일 내용을 import했다면 그 파일 안의 클래스 이름과 일치 시켜줘야 함
+      home: HomeScreen(),
     );
   }
 }
---
+++ toonflix/lib/services/api_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
+
+class ApiService {
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev/";
+  static const String today = "today";
+
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    // 해당 함수가 완전히 실행될 때까지(데이터가 다 올 때까지) 기다렸다가 넘어가라는 명령어 await(async함수 내에서 써야 함)
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+}
